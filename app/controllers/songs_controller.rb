0b0db77debc0d8994e00d7df7b9fd97a1bf90b7e
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-20 19:11:32 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -55,6 +55,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name, :genre_id, :notes => [])
+    params.require(:song).permit(:title, :artist_name, :genre_name, :notes => [])
   end
 end
