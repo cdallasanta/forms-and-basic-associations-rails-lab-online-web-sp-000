f1f449efeefeb92b41668d21f0901c1688cfeee3
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-20 18:07:21 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,11 +11,7 @@ class Song < ActiveRecord::Base
   end
 
   def genre_name
-<<<<<<< HEAD
     self.genre ? self.genre.name : nil
-=======
-    self.genre.name ? self.genre : nil
->>>>>>> 28d5c80af3049314dbf97a6e9d34a7a9b759981d
   end
 
   def artist_name=(artist_name)
@@ -23,11 +19,7 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name
-<<<<<<< HEAD
     self.artist ? self.artist.name : nil
-=======
-    self.artist.name ? self.artist : nil
->>>>>>> 28d5c80af3049314dbf97a6e9d34a7a9b759981d
   end
 
   def note_contents
@@ -41,13 +33,8 @@ class Song < ActiveRecord::Base
   end
 
   def note_contents=(new_content)
-<<<<<<< HEAD
     new_content.each do |new_note|
       self.notes << Note.create(content: new_note)
     end
-=======
-    binding.pry
-    self.notes << Note.create(content: new_content)
->>>>>>> 28d5c80af3049314dbf97a6e9d34a7a9b759981d
   end
 end
